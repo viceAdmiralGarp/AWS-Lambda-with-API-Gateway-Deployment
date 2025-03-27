91571adef9464b36396b8a87237aba8a793f8030
viceAdmiralGrap <tihonovvyacheslav636@gmail.com>
2025-03-27 23:48:33 +0200

    init

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -2,11 +2,14 @@ package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 @LambdaHandler(
     lambdaName = "hello_world",
@@ -15,13 +18,19 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+
+		response.setStatusCode(200);
+		response.setHeaders(headers);
+		response.setBody("{\"message\": \"Hello from Lambda!\"}");
+
+		return response;
 	}
 }
